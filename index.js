d50ab5f0d7d301e35b3ea0f0defdd46cd82dbfdc
Samarkun <samarkun4@gmail.com>
2025-05-07 11:03:37 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const PORT = 8000;
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
+
+
+app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
+
