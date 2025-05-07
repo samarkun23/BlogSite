cbdd5e2dfa1732c57a57aa737f7cb866ca46958e
Samarkun <samarkun4@gmail.com>
2025-05-07 17:29:23 +0530

    user model

---
+++ models/user.js
@@ -0,0 +1,82 @@
+const { createHmac , randomBytes} = require("crypto")
+const {Schema, model} = require('mongoose')
+
+const userSchema = new Schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    salt: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    profileImageUrl: {
+        type: String,
+        default: '/images/avatar',
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER"
+    }
+},{timestamps: true})
+
+userSchema.pre('save', function (next) {
+    const user = this; //this is a user
+    if (!user.isModified("password")) return;
+     
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac('sha256', salt)
+        .update(user.password)
+        .digest("hex")
+    
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next()
+})
+
+
+
+const User = model('user', userSchema)
+
+module.exports = User
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
