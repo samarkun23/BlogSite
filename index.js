2cb317782a1010a71c9a33a584878267b717ad5a
Samarkun <samarkun4@gmail.com>
2025-05-07 17:26:48 +0530

    addind a route for home

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const PORT = 8000;
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
-
+app.get('/', (req,res) => {
+    res.render('home')
+})
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
 
