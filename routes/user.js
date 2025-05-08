ab8dbddcd04d21c1b698628b443cad29a372cd25
Samarkun <samarkun4@gmail.com>
2025-05-08 13:43:53 +0530

    routes for signin

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,19 @@
 const {Router} = require('express');
-const signUp = require('../controllers/user');
+const {signUp, signin} = require('../controllers/user');
 const router = Router();
 
 router.get('/signup', (req, res) => {
     return res.render('signup')
 })
 
-router.get('/singin', (req, res) => {
+router.get('/signin', (req, res) => {
     return res.render('signin')
 })
 
 router.post("/signup", signUp)
+
+router.post('/signin', signin)
+
 
 module.exports = router;
 
