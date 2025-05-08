9e3195a99b3d29a659d28243f14ec68b65912c29
Samarkun <samarkun4@gmail.com>
2025-05-08 18:21:57 +0530

    we are returing a token insted of returing user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { createHmac , randomBytes} = require("crypto")
-const {Schema, model} = require('mongoose')
+const {Schema, model} = require('mongoose');
+const { createTokenForUser } = require("../jsonServiceForAuthentication/authentication");
 
 const userSchema = new Schema({
     fullName: {
@@ -45,7 +46,7 @@ userSchema.pre('save', function (next) {
 })
 
 //making the virtual function
-userSchema.static('matchPassword', async function(email, password){
+userSchema.static('matchPasswordAndGenerateToken', async function(email, password){
     const user = await this.findOne({email});
     if(!user) throw new Error("User not found!");
     
@@ -57,7 +58,8 @@ userSchema.static('matchPassword', async function(email, password){
     if(hashedPassword !== userProvidedHash) throw Error("Incorrect Password")
 
 
-    return user; 
+    const token = createTokenForUser(user)
+    return token; 
 })
 
 
