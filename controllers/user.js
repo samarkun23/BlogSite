f2512f96e99d29352c01bad9db063c72a71d5b5b
Samarkun <samarkun4@gmail.com>
2025-05-08 13:43:16 +0530

    controller fro signin

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,19 @@ async function signUp(req, res) {
     })
     return res.redirect("/") 
 }
-module.exports = signUp ;
+
+async function signin(req, res) {
+    const {email, password} = req.body;
+    const user = await User.matchPassword(email,password) 
+
+    console.log(user);
+    return res.redirect("/") 
+
+}
+
+
+
+module.exports = {signUp,signin} ;
 
 
 
