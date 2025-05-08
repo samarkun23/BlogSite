8b2075e464a6117b764b6da7c49f315337d1e99b
Samarkun <samarkun4@gmail.com>
2025-05-08 10:14:29 +0530

    register route for user

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 
+const userRoute = require('./routes/user')
+
 const app = express()
 const PORT = 8000;
 
@@ -10,6 +12,9 @@ app.set('views', path.resolve('./views'))
 app.get('/', (req,res) => {
     res.render('home')
 })
+
+app.use('/user', userRoute)
+
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
 
