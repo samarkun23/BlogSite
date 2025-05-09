b818bf2748efd5315dc535d29fdcaa8c1c1f2b76
Samarkun <samarkun4@gmail.com>
2025-05-09 15:11:24 +0530

    add cookieparser and middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
-
+const cookieParser = require('cookie-parser')
 
 const userRoute = require('./routes/user')
+const { checkForAuthenticationCookie } = require('./middlewares/authentication')
 
 const app = express()
 const PORT = 8000;
@@ -14,9 +15,14 @@ app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie("token"))
+
 
 app.get('/', (req,res) => {
-    res.render('home')
+    res.render('home', {
+        user: req.user,
+    })
 })
 
 app.use('/user', userRoute)
