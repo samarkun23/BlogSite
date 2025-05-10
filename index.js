81d351219bbf11e67a3c6ad6a44fe7afaba474d6
Samarkun <samarkun4@gmail.com>
2025-05-10 09:44:11 +0530

    register blog route

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 
+const Blog = require('./models/blog')
+
 const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
 const { checkForAuthenticationCookie } = require('./middlewares/authentication')
 
 const app = express()
@@ -19,14 +22,16 @@ app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
 
 
-app.get('/', (req,res) => {
+app.get('/', async (req,res) => {
+    const allBlogs = await Blog.find({}); 
     res.render('home', {
         user: req.user,
+        blogs: allBlogs,
     })
 })
 
 app.use('/user', userRoute)
-
+app.use('/blog', blogRoute)
 
 app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
 
